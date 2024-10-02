7c08369fcc0e6523cc58e5275a142eabf6a8ef5b
SANJAY-KUMAR06 <s3441508@gmail.com>
2024-10-02 21:33:48 +0530

    Final commit

--- main.py
+++ main.py
@@ -268,7 +268,7 @@ def contact():
                                 msg=f"Subject:Blog Contact Form\n\n Name: {request.form['name']}\n\n"
                                     f"Phone: {request.form['phone']}\n\n"
                                     f"Message: {request.form['message']}\n\n")
-            flash("Mail sent,Will touch With You Soon..")
+            flash("Mail sent,Will get touch With You Soon..")
             return redirect(url_for('contact'))
     return render_template("contact.html", current_user=current_user)
 
